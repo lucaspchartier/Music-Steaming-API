5054175f74115afd1cee15398f8a4b5be3262ddf
lucaspchartier <lucaspchartier@gmail.com>
2018-12-07 11:47:56 -0500

    Fixed up some minor linter warnings.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -42,6 +42,7 @@ class ArtistsController < ProtectedController
   end
 
   private
+
     # Use callbacks to share common setup or constraints between actions.
     def set_artist
       @artist = current_user.artists.find(params[:id])
